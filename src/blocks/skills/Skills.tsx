0d0dba98cc586431b26375df24300e54212b7ea1
Sibiryakov090591 <Sibir_91@list.ru>
2021-10-07 10:34:37 +0300

    added anomation to skills

--- src/blocks/skills/Skills.tsx
+++ src/blocks/skills/Skills.tsx
@@ -10,6 +10,24 @@ import api_logo from "../../images/skills/api_rest.png";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+const iconAnimation = (ref: any) => {
+  gsap.from(ref.current, {
+    scrollTrigger: ref.current,
+    x: "-25px",
+    opacity: 0,
+    duration: 1.3,
+  });
+};
+
+const textAnimation = (ref: any) => {
+  gsap.from(ref.current, {
+    scrollTrigger: ref.current,
+    y: "25px",
+    opacity: 0,
+    duration: 1.3,
+  });
+};
+
 const Skills: React.FC = () => {
   const classes = useStyles();
 
@@ -19,38 +37,25 @@ const Skills: React.FC = () => {
   const skill_4 = useRef<HTMLDivElement>(null);
   const skill_5 = useRef<HTMLDivElement>(null);
 
+  const text_1 = useRef<HTMLDivElement>(null);
+  const text_2 = useRef<HTMLDivElement>(null);
+  const text_3 = useRef<HTMLDivElement>(null);
+  const text_4 = useRef<HTMLDivElement>(null);
+  const text_5 = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-    gsap.from(skill_1.current, {
-      scrollTrigger: skill_1.current,
-      y: "20px",
-      opacity: 0,
-      duration: 0.6,
-    });
-    gsap.from(skill_2.current, {
-      scrollTrigger: skill_2.current,
-      y: "20px",
-      opacity: 0,
-      duration: 0.6,
-    });
-    gsap.from(skill_3.current, {
-      scrollTrigger: skill_3.current,
-      y: "20px",
-      opacity: 0,
-      duration: 0.6,
-    });
-    gsap.from(skill_4.current, {
-      scrollTrigger: skill_4.current,
-      y: "20px",
-      opacity: 0,
-      duration: 0.6,
-    });
-    gsap.from(skill_5.current, {
-      scrollTrigger: skill_5.current,
-      y: "20px",
-      opacity: 0,
-      duration: 0.6,
-    });
+    iconAnimation(skill_1);
+    iconAnimation(skill_2);
+    iconAnimation(skill_3);
+    iconAnimation(skill_4);
+    iconAnimation(skill_5);
+
+    textAnimation(text_1);
+    textAnimation(text_2);
+    textAnimation(text_3);
+    textAnimation(text_4);
+    textAnimation(text_5);
   });
 
   return (
@@ -58,36 +63,36 @@ const Skills: React.FC = () => {
       <Container className={classes.skillsContainer}>
         <Title title="Skills" index="01" />
         <div>
-          <div ref={skill_1} className={classes.wrapper}>
-            <div className={classes.iconWrapper}>
+          <div className={classes.wrapper}>
+            <div ref={skill_1} className={classes.iconWrapper}>
               <img className={classes.icon} src={react_logo} alt="React.js" />
             </div>
-            <p className={classes.description}>
+            <p ref={text_1} className={classes.description}>
               I often use next technologies: React-Redux for state managing,
               React-Router-Dom for work with routs, Redux-Form and Formik,
               Redux-Thunk for side-effects.
             </p>
           </div>
 
-          <div ref={skill_2} className={classes.wrapper}>
-            <div className={classes.iconWrapper}>
+          <div className={classes.wrapper}>
+            <div ref={skill_2} className={classes.iconWrapper}>
               <img
                 className={classes.icon}
                 src={typescript_logo}
                 alt="TypeScript"
               />
             </div>
-            <p className={classes.description}>
+            <p ref={text_2} className={classes.description}>
               I like statically typed languages because there is so much more
               time left to code than fix bugs.
             </p>
           </div>
 
-          <div ref={skill_3} className={classes.wrapper}>
-            <div className={classes.iconWrapper}>
+          <div className={classes.wrapper}>
+            <div ref={skill_3} className={classes.iconWrapper}>
               <img className={classes.icon} src={sass_logo} alt="SASS" />
             </div>
-            <p className={classes.description}>
+            <p ref={text_3} className={classes.description}>
               I definitely use sumantic HTML tags, for cross-browser
               compatibility i use normalize.css and and check the browser
               support for the tags used (Hello IE!...) And I really enjoy
@@ -95,21 +100,21 @@ const Skills: React.FC = () => {
             </p>
           </div>
 
-          <div ref={skill_4} className={classes.wrapper}>
-            <div className={classes.iconWrapper}>
+          <div className={classes.wrapper}>
+            <div ref={skill_4} className={classes.iconWrapper}>
               <img className={classes.icon} src={gsap_logo} alt="GSAP" />
             </div>
-            <p className={classes.description}>
+            <p ref={text_4} className={classes.description}>
               I like greensock (GSAP) library for animation. Today I do easy
               animation, but i like this and future i want up level the skill.
             </p>
           </div>
 
-          <div ref={skill_5} className={classes.wrapper}>
-            <div className={classes.iconWrapper}>
+          <div className={classes.wrapper}>
+            <div ref={skill_5} className={classes.iconWrapper}>
               <img className={classes.icon} src={api_logo} alt="REST API" />
             </div>
-            <p className={classes.description}>
+            <p ref={text_5} className={classes.description}>
               For HTTP requests (CRUD operations) I like axios library.
             </p>
           </div>
