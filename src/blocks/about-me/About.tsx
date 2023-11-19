bc64fc39316fd203c0e04a08f1ea8531180df1da
andrey <andrey.sibiriakov@chipassist.com>
2023-11-19 16:47:09 -0600

    fixed about section content

--- src/blocks/about-me/About.tsx
+++ src/blocks/about-me/About.tsx
@@ -5,6 +5,7 @@ import { Container } from "@material-ui/core";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useI18n } from "../../services/I18next";
+import about from "../../images/about.png";
 
 const About: React.FC = () => {
   const classes = useStyles();
@@ -18,7 +19,12 @@ const About: React.FC = () => {
     <section id="about" className={classes.root}>
       <Container className={classes.container}>
         <Title title={t("menu.about")} index="01" />
-        <div className={classes.content}>Lorem</div>
+        <div className={classes.content}>
+          <div>{t("text")}</div>
+          <div>
+            <img src={about} alt="about" />
+          </div>
+        </div>
       </Container>
     </section>
   );
--- src/blocks/about-me/styles.ts
+++ src/blocks/about-me/styles.ts
@@ -1,8 +1,9 @@
 import { makeStyles } from "@material-ui/styles";
 import { AppTheme } from "../../themes/paletteTypes";
 import background from "../../images/background/blackgrit-6a2d636a530802d3ce97afb7af53a84b.png";
+import { Theme } from "@material-ui/core";
 
-export const useStyles = makeStyles((theme: AppTheme) => ({
+export const useStyles = makeStyles((theme: Theme & AppTheme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
     backgroundImage: `url(${background})`,
@@ -17,8 +18,22 @@ export const useStyles = makeStyles((theme: AppTheme) => ({
   content: {
     color: theme.palette.text.secondary,
     fontWeight: "bold",
-    fontSize: "1.1rem",
-    lineHeight: 1.4,
+    fontSize: "1.2rem",
+    lineHeight: 1.6,
+    maxWidth: 800,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column-reverse",
+    },
+    "& img": {
+      filter: "drop-shadow(0px 0px 25px #64ffda)",
+      marginLeft: 50,
+      [theme.breakpoints.down("sm")]: {
+        display: "none",
+      },
+    },
   },
 }));
 
